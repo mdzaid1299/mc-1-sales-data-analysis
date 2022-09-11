276a9825549e6b7778a81f555f200a6f041c4ef1
HP <mdzaid1299@gmail.com>
2022-09-12 00:12:01 +0530

    initial

--- src/main/java/com/jap/sales/SalesDataAnalyzer.java
+++ src/main/java/com/jap/sales/SalesDataAnalyzer.java
@@ -11,15 +11,40 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SalesDataAnalyzer {
-   public List<SalesRecord> readFile(String fileName) {
-       return null;
+    public List<SalesRecord> readFile(String fileName) {
+        List<SalesRecord> recordList = new ArrayList<>();
+        try{
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String data = bufferedReader.readLine();
+            while((data = bufferedReader.readLine()) != null){
+                String[] val = data.split(",");
+                String date = val[0];
+                int customer_id = Integer.parseInt(val[1]);
+                int product_category = Integer.parseInt(val[2]);
+                String payment_method = val[3];
+                double amount = Double.parseDouble(val[4]);
+                double time_on_site = Double.parseDouble(val[5]);
+                int clicks_in_site = Integer.parseInt(val[6]);
+                recordList.add(new SalesRecord(date,customer_id,product_category,payment_method,amount,time_on_site,clicks_in_site));
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return recordList;
     }
 
+
+
     public List<SalesRecord> getAllCustomersSortedByPurchaseAmount(List<SalesRecord> salesData){
-      return null;
+        Comparator<SalesRecord> recordComparator = (a1,a2) -> (int) (a2.getAmount() - a1.getAmount());
+        Collections.sort(salesData,recordComparator);
+        return salesData;
     }
     public SalesRecord getTopCustomerWhoSpentMaxTimeOnSite(List<SalesRecord> salesData){
-       return null;
+        Comparator<SalesRecord> recordComparator = (a1,a2) -> (int)(a2.getTime_on_site() - a1.getTime_on_site());
+        Collections.sort(salesData,recordComparator);
+        return salesData.get(0);
     }
 
 
